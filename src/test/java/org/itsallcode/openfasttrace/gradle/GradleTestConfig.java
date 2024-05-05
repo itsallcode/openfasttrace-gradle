c7988407acfa40bc4f1d3599bfacfee93adaa044
kaklakariada <kaklakariada@users.noreply.github.com>
2024-05-05 18:30:43 +0200

    Upgrade dependencies

--- src/test/java/org/itsallcode/openfasttrace/gradle/GradleTestConfig.java
+++ src/test/java/org/itsallcode/openfasttrace/gradle/GradleTestConfig.java
@@ -6,7 +6,7 @@ public enum GradleTestConfig
     /**
      * Version 8.5 is the first one that supports running on Java 21
      */
-    EIGHT_FIVE("8.5"), EIGHT_SIX("8.6");
+    EIGHT_FIVE("8.5"), EIGHT_SIX("8.6"), EIGHT_SEVEN("8.7");
 
     public final String gradleVersion;
 
