b5db0153a048d280b0999e0fe2b8934ad7578a80
kaklakariada <github@chp1.net>
2026-02-09 10:51:06 +0800

    Upgrade dependencies

--- src/test/java/org/itsallcode/openfasttrace/gradle/GradleTestConfig.java
+++ src/test/java/org/itsallcode/openfasttrace/gradle/GradleTestConfig.java
@@ -1,17 +1,15 @@
 package org.itsallcode.openfasttrace.gradle;
 
-public enum GradleTestConfig
-{
+public enum GradleTestConfig {
     THIS_VERSION(null),
     /**
      * We support the latest Gradle version and the previous two.
      */
-    PREVIOUS_VERSION_MINUS_ONE("8.12"), PREVIOUS_VERSION("8.13"), CURRENT_VERSION("8.14");
+    PREVIOUS_VERSION_MINUS_ONE("7.6.6"), PREVIOUS_VERSION("8.14.1"), CURRENT_VERSION("9.3.1");
 
     public final String gradleVersion;
 
-    private GradleTestConfig(final String gradleVersion)
-    {
+    private GradleTestConfig(final String gradleVersion) {
         this.gradleVersion = gradleVersion;
     }
 }
