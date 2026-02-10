e9ddff33ea233569acef856c51c31590cc68fe0c
kaklakariada <github@chp1.net>
2026-02-10 16:17:57 +0800

    Fix gradle versions

--- src/test/java/org/itsallcode/openfasttrace/gradle/GradleTestConfig.java
+++ src/test/java/org/itsallcode/openfasttrace/gradle/GradleTestConfig.java
@@ -6,7 +6,7 @@ public enum GradleTestConfig {
      * We support the latest Gradle version and the previous two. Older Gradle
      * versions do not work with Java 21 and above.
      */
-    PREVIOUS_VERSION_MINUS_ONE("9.1"), PREVIOUS_VERSION("9.2"), CURRENT_VERSION("9.3.1");
+    PREVIOUS_VERSION_MINUS_ONE("9.1.0"), PREVIOUS_VERSION("9.2.0"), CURRENT_VERSION("9.3.1");
 
     public final String gradleVersion;
 
