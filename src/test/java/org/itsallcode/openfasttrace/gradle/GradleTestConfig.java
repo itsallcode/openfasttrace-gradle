c807be32e640a69a513a43a66bf54132e3e6f914
kaklakariada <github@chp1.net>
2026-02-12 11:13:38 +0800

    Adapt test assertion

--- src/test/java/org/itsallcode/openfasttrace/gradle/GradleTestConfig.java
+++ src/test/java/org/itsallcode/openfasttrace/gradle/GradleTestConfig.java
@@ -1,16 +1,20 @@
 package org.itsallcode.openfasttrace.gradle;
 
-public enum GradleTestConfig {
+public enum GradleTestConfig
+{
     THIS_VERSION(null),
+
     /**
      * We support the latest Gradle version and the previous two. Older Gradle
      * versions do not work with Java 21 and above.
      */
-    PREVIOUS_VERSION_MINUS_ONE("9.1.0"), PREVIOUS_VERSION("9.2.0"), CURRENT_VERSION("9.3.1");
+    ; // PREVIOUS_VERSION_MINUS_ONE("9.1.0"), PREVIOUS_VERSION("9.2.0"),
+      // CURRENT_VERSION("9.3.1");
 
     public final String gradleVersion;
 
-    private GradleTestConfig(final String gradleVersion) {
+    private GradleTestConfig(final String gradleVersion)
+    {
         this.gradleVersion = gradleVersion;
     }
 }
